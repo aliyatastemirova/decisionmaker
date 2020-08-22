b93b0efde408062c87248acc55f321aad95777d5
aliyatas <39190883+aliyatas@users.noreply.github.com>
2020-08-22 16:22:03 +0200

    Added comments and documentation

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # decisionmaker
+
+## Setup
+
+### Installation
+
+**Requirements**
+
+- Python 3.8.*
+- Pip
+- All the required libraries installed (Installation described below)
+
+### Environment setup
+
+Setting up the environment with requirements.txt:
+
+- Run `pip install -r requirements.txt` or `pip install --user --requirement requirements.txt`
+- If you want to update requirements file, add necessary packages and run `pip freeze > requirements.txt`
+
+Run usual django commands (manage.py file is in the main **decisionmaker** directory):
+
+- `python manage.py makemigrations`
+- `python manage.py migrate`
+- `python manage.py migrate --run-syncdb`
+- `python manage.py runserver`
+
+### About the website
+
+Includes one django app: questions
+
+This app allows a user to enter a question and add all possible options and get a random option chosen to help make a decision.
+It's kind of a coin flip, but with a possibility to randomly choose from a wide range of options.
+
+Design/files:
+- static directory includes bootstrap css, js, and custom js necessary for templates and favicon (static/images)
+- templates extend base template (scripts, main design)
+
+Technical details:
+- This is a model-free app, all data is stored in sessions
+- There are only two views: main (homepage) and result
+- There are two forms specified in forms.py: question and answers
+- .gitattributes file is there to make sure github marks this repo as python
--- decisionmaker/questions/views.py
+++ decisionmaker/questions/views.py
@@ -1,58 +1,57 @@
 from django.shortcuts import render
 from django.views import View
-from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import Question, Answers
 from django.forms.formsets import formset_factory
 import random
 
 
-# Create your views here.
-# class HomeView(View):
-#     """
-#     Homepage view, default view of the website
-#     """
-#     template_name = "questions/homepage.html"
-#
-#     def get(self, request, *args, **kwargs):
-#         return render(request, self.template_name)
-
-
 class HomeView(View):
+    """
+    Homepage view, the default view of the page. In order to allow a user add many answers we need two separate forms
+    for the question and for answers
+    """
     template_name = "questions/homepage.html"
     form = Question
-    QuestionsFormSet = formset_factory(Answers, extra=2)
+    AnswersFormSet = formset_factory(Answers, extra=2)
 
     def get(self, request, *args, **kwargs):
+        """
+        Questions and answers are stored in request.session, which is flushed when the page is loading, so that a user
+        can ask about new things
+        """
         request.session.flush()
-        form = self.form()
-        formset = self.QuestionsFormSet()
+        form = self.form()  # Question form
+        formset = self.AnswersFormSet()  # Answers formset
         return render(request, self.template_name, {'form': form, "formset": formset})
 
     def post(self, request, *args, **kwargs):
         form = self.form(request.POST)
-        formset = self.QuestionsFormSet(request.POST)
+        formset = self.AnswersFormSet(request.POST)
         if formset.is_valid() and form.is_valid():
-            answer_list = [frm.cleaned_data.get('answer') for frm in formset]
-            form = form.cleaned_data
-            request.session["answers"] = answer_list
-            request.session["question"] = form["question"]
+            answer_list = [frm.cleaned_data.get('answer') for frm in formset]  # get list of answers from clean formset
+            form = form.cleaned_data  # get clean question form
+            request.session["answers"] = answer_list  # store answers in session
+            request.session["question"] = form["question"]  # store the question in session
         else:
             form = self.form()
-            formset = self.QuestionsFormSet()
+            formset = self.AnswersFormSet()
         return HttpResponseRedirect('/result/', {'form': form, "formset": formset})
 
 
 class ResultView(View):
+    """
+    Results page that shows the asked question and the chosen answer
+    """
     template_name = "questions/result.html"
 
     def get(self, request, *args, **kwargs):
-        if "answers" not in request.session.keys():
+        if "answers" not in request.session.keys():  # in case someone tries to open /result directly
             return HttpResponseRedirect('/')
         else:
-            answers = request.session["answers"]
+            answers = request.session["answers"]  # retrieve data from the session
             question = request.session["question"]
-            chosen_answer = random.choice(answers)
+            chosen_answer = random.choice(answers)  # randomly choose an option for the user
             context = {
                 "answer": chosen_answer,
                 "question": question
