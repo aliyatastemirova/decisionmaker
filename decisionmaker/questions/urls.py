316b4261bccbc3131a9b59efaca5480e3cec915d
aliyatas <39190883+aliyatas@users.noreply.github.com>
2020-08-22 15:40:17 +0200

    Added custom js file, dynamic form

--- decisionmaker/questions/urls.py
+++ decisionmaker/questions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ResultView
+
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home")
+    path("", HomeView.as_view(), name="home"),
+    path("result/", ResultView.as_view(), name="result")
 ]
--- decisionmaker/questions/views.py
+++ decisionmaker/questions/views.py
@@ -1,14 +1,50 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib import messages
+from formtools.wizard.views import SessionWizardView, WizardView
+from django.http import HttpResponseRedirect
+from .forms import Question, Answers
+from django.forms.formsets import formset_factory
 
 
 # Create your views here.
+# class HomeView(View):
+#     """
+#     Homepage view, default view of the website
+#     """
+#     template_name = "questions/homepage.html"
+#
+#     def get(self, request, *args, **kwargs):
+#         return render(request, self.template_name)
+
+
 class HomeView(View):
-    """
-    Homepage view, default view of the website
-    """
     template_name = "questions/homepage.html"
+    form = Question
+    QuestionsFormSet = formset_factory(Answers, extra=2)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        formset = self.QuestionsFormSet()
+        return render(request, self.template_name, {'form': form, "formset": formset})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        formset = self.QuestionsFormSet(request.POST)
+        if formset.is_valid() and form.is_valid():
+            answer_list = [frm.cleaned_data.get('answer') for frm in formset]
+            form = form.cleaned_data
+            request.session["answers"] = answer_list
+            request.session["question"] = form["question"]
+            print(request.session["answers"])
+        else:
+            form = self.form()
+            formset = self.QuestionsFormSet()
+        return HttpResponseRedirect('/result/', {'form': form, "formset": formset})
+
+
+class ResultView(View):
+    template_name = "questions/result.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
